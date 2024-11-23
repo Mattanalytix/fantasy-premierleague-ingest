f73c99b4eb90fa041f660ad250a909ba27b569a5
matthh9797 <matthewh@netextremity.com>
2024-11-23 12:16:16 +0100

    Added application configuration file and functions

---
+++ config.py
@@ -0,0 +1,49 @@
+import logging
+import yaml
+
+
+def load_config() -> dict:
+    """
+    load application config into dict
+    @return application config dict
+    """
+    with open('config.yml', 'r') as file:
+        return yaml.safe_load(file)
+
+
+def validate_config_endpoint(config_endpoint: dict) -> None:
+    """
+    validate the configuration for an endpoint looks as expected
+    @param config_endpoint the endpoint configuration dict
+    """
+    config_endpoint_error_keys = {'name', 'config', 'tables'} - set(
+        config_endpoint.keys())
+    assert len(config_endpoint_error_keys) == 0, f"""
+        Endpoint configuration for {config_endpoint['name']} missing required
+        values {config_endpoint_error_keys}"""
+
+    try:
+        config_endpoint_warning_keys = {'description'} - set(
+            config_endpoint.keys())
+        assert len(config_endpoint_warning_keys) == 0, f"""Endpoint
+            configuration for {config_endpoint['name']} missing desired values
+            {config_endpoint_warning_keys}"""
+    except AssertionError as e:
+        logging.warning(e)
+
+
+def get_table_config(table: dict, default_config: dict) -> dict:
+    """
+    get table config and overwrite defaults if config set for table
+    @param table config for the table
+    @param default_config default config for all tables
+    @return table config with defaults
+    """
+    table_config = table.get('config', {})
+    for k, v in default_config.items():
+        if k not in table_config:
+            table_config[k] = v
+        else:
+            logging.debug("""Overwritting default config for '%s' to %s for
+                          table %s""", k, v, table['name'])
+    return table_config
